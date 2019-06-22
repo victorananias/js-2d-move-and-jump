3f986af29f501eb030c88da47a6524d244e4387b
victor <victor@ananias.dev>
2019-06-22 10:47:13 -0300

    let > const

--- js/teclado.js
+++ js/teclado.js
@@ -1,53 +1,53 @@
-let SETA_ESQUERDA = 37;
-let SETA_DIREITA = 39;
-let ESPACO = 32;
+const SETA_ESQUERDA = 37;
+const SETA_DIREITA = 39;
+const ESPACO = 32;
 
 
-function Teclado(elemento) {
-  // Recebe o document
-  this.elemento = elemento;
+class Teclado {
+  
+  constructor(elemento) {
+    // Recebe o document
+    this.elemento = elemento;
 
-  // Array de teclas pressionadas
-  this.pressionadas = [];
-
-  // Array de teclas disparadas
-  this.disparadas = [];
-
-  // Funções de disparo
-  this.funcoesDisparo = [];
-
-  // Registrando teclas no Array
-  let teclado = this;
-
-  // Define a tecla pressionada como true
-  elemento.addEventListener("keydown", function(evento) {
-    let tecla = evento.keyCode; // Tornando mais legível
-    teclado.pressionadas[tecla] = true;
-
-    // Dispara somente se for o primeiro clique da tecla
-    // Caso a tecla seja mantida pressionada, execução interrompida
-    if(teclado.funcoesDisparo[tecla] && !teclado.disparadas[tecla]) {
-      teclado.disparadas[tecla] = true;
-      // Executa a função assossiada no array
-      teclado.funcoesDisparo[tecla] ();
-    }
-  });
-
-  // Define a tecla pressionada como false
-  elemento.addEventListener("keyup", function(evento) {
-    let tecla = evento.keyCode; // Tornando mais legível
-    teclado.pressionadas[tecla] = false;
-    // Torna possível disparar novamente
-    teclado.disparadas[tecla] = false;
-  });
-}
-
-
-Teclado.prototype = {
-  pressionada: function(tecla) {
+    // Array de teclas pressionadas
+    this.pressionadas = [];
+  
+    // Array de teclas disparadas
+    this.disparadas = [];
+  
+    // Funções de disparo
+    this.funcoesDisparo = [];
+    
+    elemento.addEventListener("keydown", (evento) => {
+      const tecla = evento.keyCode; // Tornando mais legível
+      this.pressionadas[tecla] = true;
+  
+      // Dispara somente se for o primeiro clique da tecla
+      // Caso a tecla seja mantida pressionada, execução interrompida
+      if(this.funcoesDisparo[tecla] && !this.disparadas[tecla]) {
+        this.disparadas[tecla] = true;
+        // Executa a função assossiada no array
+        this.funcoesDisparo[tecla]();
+      }
+    });
+    
+    // Define a tecla pressionada como true
+  
+    // Define a tecla pressionada como false
+    elemento.addEventListener("keyup", (evento) => {
+      let tecla = evento.keyCode; // Tornando mais legível
+      this.pressionadas[tecla] = false;
+      // Torna possível disparar novamente
+      this.disparadas[tecla] = false;
+    });
+  }
+  
+  pressionada(tecla) {
     return this.pressionadas[tecla];
-  },
-  disparou: function(tecla, callback) {
+  }
+  
+  disparou(tecla, callback) {
     this.funcoesDisparo[tecla] = callback;
   }
+
 }
